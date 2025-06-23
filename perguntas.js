763dc23c31cccd9c9f665f529df2b093b68fd454
FreitasMurilo1A <167330203+FreitasMurilo1A@users.noreply.github.com>
2025-06-23 11:08:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Ciência',
+    'A que temperatura a água ferve?',
+    '100ºC'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde se localiza Machu Picchu?',
+    'Peru'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin'
+)
